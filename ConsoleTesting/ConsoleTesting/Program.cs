1f3d181638f38a7b1b47ba359cc5d37a9241c93a
vision668 <v-mench@outlook.com>
2018-01-07 10:27:41 +0800

    demo

---
+++ ConsoleTesting/ConsoleTesting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("myfirstte\tmyfirst test");
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# helloworld
+This is my fist repository for learning git
+refert
+I want to commit to origin/master-branch01
+
+second test
